e63491a2e7796ed01090fe6e2a91e5f79abef9bc
Willianfloripa <willian.tonel@gmail.com>
2026-08-15 01:38:25 -0300

    versao

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,7 +13,7 @@ import { LoadingService } from '../../services/loading.service';
 })
 export class HomeComponent {
   isLoading: boolean = false;
-  versao: string = '4.1.0';
+  versao: string = '4.1.1';
 
   constructor(
     private router: Router,
